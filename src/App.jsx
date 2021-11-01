56e873a8f693f28be8911316d866b2af7c966002
throne.yash@gmail.com <throne.yash@gmail.com>
2021-11-01 13:35:23 -0600

    Updating App.js

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fullName: '',
+      username: '',
+      email: '',
+      password: '',
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="form-div">
+            <form action="">
+              <input
+                type="text"
+                placeholder="Full Name"
+                onChange={this.changeFullName}
+                value={this.state.fullName}
+                className="form-control form-group"
+              />
+
+              <input
+                type="text"
+                placeholder="Username"
+                onChange={this.changeUsername}
+                value={this.state.username}
+                className="form-control form-group"
+              />
+
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={this.changeEmail}
+                value={this.state.email}
+                className="form-control form-group"
+              />
+              <input
+                type="text"
+                placeholder="Password"
+                onChange={this.changePassword}
+                value={this.state.password}
+                className="form-control form-group"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
